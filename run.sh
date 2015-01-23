13e79083579eb9fab4c0be6a553f0c15e9602e63
Horvan <drubuntu@outlook.com>
2015-01-23 16:06:29 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "You'll install Drubuntu now! Enter your passwort  or hit CTRL+C to abbort installation"
 apt -y install git
 git clone https://github.com/drubuntu/installation $HOME/installation
 cd $HOME/installation
 bash install.sh 2>$HOME/errors.txt
-rm -r $HOME/installation
-rm -- "$0"
+rm -r "$HOME"/installation
