46bec3d6ff2068fa297f868d909d7c1ce39e47ee
Horvan <drubuntu@outlook.com>
2015-01-24 06:49:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'Dpkg::Progress-Fancy "1";' > /etc/apt/apt.conf.d/99progressbar
-apt -y install git
+
+sudo apt -y install git
 git clone https://github.com/drubuntu/installation $HOME/installation
 cd $HOME/installation
 bash install.sh 2>$HOME/errors.txt
